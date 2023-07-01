ef1e70d45d3cb99c984ca381526e5565493f00fa
Gisele Cortes <86819936+giseleCortes@users.noreply.github.com>
2023-07-01 10:40:04 -0300

    feat: Adicionado MUI

--- retomadasprojeto/src/routes/index.tsx
+++ retomadasprojeto/src/routes/index.tsx
@@ -1,11 +1,12 @@
 
-import {Routes, Route, Navigate} from 'react-router-dom'; 
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom'; 
 
 export const AppRoutes = () => {
     return (
 
         <Routes>
-            <Route path='/pagina-inicial'element={<p> Página Inicial </p>} />
+            <Route path='/pagina-inicial'element= {<Button>Teste</Button>} />
 
             <Route path='*' element={<Navigate to= "/pagina-inicial" /> } />
         </Routes>
