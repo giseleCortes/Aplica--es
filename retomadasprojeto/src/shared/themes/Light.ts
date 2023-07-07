7c3ac1a422e3a4346b3a9a036dfa840ac81ab576
Gisele Cortes <86819936+giseleCortes@users.noreply.github.com>
2023-07-07 16:00:52 -0300

    style: corrigido nome verificado cor no tema light

--- retomadasprojeto/src/shared/themes/Light.ts
+++ retomadasprojeto/src/shared/themes/Light.ts
@@ -26,7 +26,7 @@ export const LightTheme = createTheme({
     },
          typography:{
             allVariants:{
-                color: 'dodgerblue',
+                color: 'darkslategray',
             }
 
         }
